dce0300cc2609fdaa929fbed0ee515c895177062
Строков Артур Александрович <AAStrokov@alfabank.ru>
2024-06-04 10:36:50 +0300

    start project

---
+++ src/test/java/com/gmail/arthurstrokov/keycloak/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.arthurstrokov.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
